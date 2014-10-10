727cbe8df647e364c178df529dd79bac2e84feff
yangboz <YoungWelle@gmail.com>
2014-10-10 12:55:13 +0800

    ADDED:ApplicationContextMain example files;

--- src/com/godpaper/javapostsforlearning/ApplicationContextMain.java
+++ src/com/godpaper/javapostsforlearning/ApplicationContextMain.java
@@ -7,24 +7,20 @@ package com.godpaper.javapostsforlearning;
  * @author yangboz
  *
  */
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 
-public class BeanAutowiringByNameMain {
+public class ApplicationContextMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("/main/resources/ApplicationContext.xml");  
-		  
-        Country countryObj = (Country) appContext.getBean("country");  
-        String countryName=countryObj.getCountryName();  
-//        String capitalName=countryObj.getCapital().getCapitalName();  
-//        System.out.println(capitalName+" is capital of "+countryName);  
-
+		  Country countryObj = (Country) appContext.getBean("country");  
+		  System.out.println("Capital Name:"+countryObj.getCapitalName("capital"));
 	}
 
 }
--- src/com/godpaper/javapostsforlearning/Country.java
+++ src/com/godpaper/javapostsforlearning/Country.java
@@ -1,21 +1,34 @@
 package com.godpaper.javapostsforlearning;
-import java.util.List;
-import java.lang.String;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 public class Country {
-	String countryName;  
-    Capital capital;  
-    public String getCountryName() {  
-        return countryName;  
-    }  
-    public void setCountryName(String countryName) {  
-        this.countryName = countryName;  
-    }  
-    public Capital getCapital() {  
-        return capital;  
-    }  
-    public void setCapital(Capital capital) {  
-        this.capital = capital;  
-    }  
+	
+	String countryName ;  
+	 ApplicationContext applicationContext;  
+	 Capital capital;  
+	 public String getCountryName() {  
+	  return countryName;  
+	 }  
+	   
+	   
+	 public void setCountryName(String countryName) {  
+	  this.countryName = countryName;  
+	 }  
+	  
+	  
+	 public void setApplicationContext(ApplicationContext applicationContext)  
+	   throws BeansException {  
+	  this.applicationContext=applicationContext;  
+	 }  
+	   
+	 public String getCapitalName(String capitalBeanName)  
+	 {  
+	  capital=(Capital) applicationContext.getBean(capitalBeanName);  
+	  String capitalName=capital.getCapitalName();  
+	  return capitalName;  
+	 }
 
 }
